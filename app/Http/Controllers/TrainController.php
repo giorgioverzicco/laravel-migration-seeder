136386ed1d80436b4d33fd0386a983bffdd6c519
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-07-12 15:16:22 +0200

    add today departing trains

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+    {
+        $startDate = date('Y-m-d 00:00:00');
+        $endDate = date('Y-m-d 23:59:59');
+
+        $trains = Train::whereBetween('departure_time', [$startDate, $endDate])
+            ->get();
+
+        dd($trains);
+    }
 }
